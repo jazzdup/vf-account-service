d6d6ffbf634be29bb221ad09afe2c8821e8f810d
Ravi Aghera <ravi.aghera@vodafone.com>
2017-12-11 11:09:10 +0000

    First commit of project

---
+++ src/main/java/com/vodafone/charging/vfaccountservice/VfAccountServiceApplication.java
@@ -0,0 +1,12 @@
+package com.vodafone.charging.vfaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfAccountServiceApplication.class, args);
+	}
+}
