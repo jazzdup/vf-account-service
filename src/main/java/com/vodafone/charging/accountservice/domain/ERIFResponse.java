ceaca9e16c903fbf678ca5dd91245eb7548efc35
alexromanin <alex.romanin1@vodafone.com>
2018-01-16 14:04:39 +0000

    removed setters

--- src/main/java/com/vodafone/charging/accountservice/domain/ERIFResponse.java
+++ src/main/java/com/vodafone/charging/accountservice/domain/ERIFResponse.java
@@ -24,79 +24,40 @@ public class ERIFResponse {
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public String getBan() {
         return ban;
-    }
-
-    public void setBan(String ban) {
-        this.ban = ban;
     }
 
     public String getErrId() {
         return errId;
     }
 
-    public void setErrId(String errId) {
-        this.errId = errId;
-    }
-
     public String getErrDescription() {
         return errDescription;
-    }
-
-    public void setErrDescription(String errDescription) {
-        this.errDescription = errDescription;
     }
 
     public Integer getBillingCycleDay() {
         return billingCycleDay;
     }
 
-    public void setBillingCycleDay(Integer billingCycleDay) {
-        this.billingCycleDay = billingCycleDay;
-    }
-
     public String getServiceProviderId() {
         return serviceProviderId;
-    }
-
-    public void setServiceProviderId(String serviceProviderId) {
-        this.serviceProviderId = serviceProviderId;
     }
 
     public String getChildServiceProviderId() {
         return childServiceProviderId;
     }
 
-    public void setChildServiceProviderId(String childServiceProviderId) {
-        this.childServiceProviderId = childServiceProviderId;
-    }
-
     public String getServiceProviderType() {
         return serviceProviderType;
-    }
-
-    public void setServiceProviderType(String serviceProviderType) {
-        this.serviceProviderType = serviceProviderType;
     }
 
     public boolean isPrepay() {
         return isPrepay;
     }
 
-    public void setPrepay(boolean prepay) {
-        isPrepay = prepay;
-    }
-
     public List<String> getUsergroups() {
         return usergroups;
     }
 
-    public void setUsergroups(List<String> usergroups) {
-        this.usergroups = usergroups;
-    }
 }
