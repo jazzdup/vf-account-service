bfca9fdbea6a60a570453d0b795719c45447b779
Ravi Aghera <ravi.aghera@vodafone.com>
2018-02-05 15:02:45 +0000

    comments on code

--- src/main/java/com/vodafone/charging/accountservice/domain/enums/ValidateHttpHeaderName.java
+++ src/main/java/com/vodafone/charging/accountservice/domain/enums/ValidateHttpHeaderName.java
@@ -2,10 +2,11 @@ package com.vodafone.charging.accountservice.domain.enums;
 
 /**
  * Headers required to be passed to ER IF.
+ * Most of these should arrive in the body of incoming post request.
  */
 public enum ValidateHttpHeaderName {
 
-    TARGET_HEADER_NAME("x-vf-target"),
+    TARGET_HEADER_NAME("x-vf-target" ),
     COUNTRY_HEADER_NAME("country"),
     ENVIRONMENT_PROPERTY_SERVER_NAME("environment"),
     REQUEST_CHARGING_ID_HEADER_NAME("x-vf-charging-identifier"),
@@ -13,6 +14,7 @@ public enum ValidateHttpHeaderName {
     REQUEST_CLIENT_ID_HEADER_NAME("x-clientid"),
     REQUEST_PARTNER_ID_HEADER_NAME("x-partnerid"),
     REQUEST_PACKAGE_ID_HEADER_NAME("x-packageid"),
+    //Currently this is just "VALIDATE".
     REQUEST_CLASS_HEADER_NAME("x-vf-request-class");
 
     ValidateHttpHeaderName(String headerName) {
