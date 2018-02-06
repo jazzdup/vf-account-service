a42f31ee0c41f242ef722a0fd1cdf15cf9de9eda
Ravi Aghera <ravi.aghera@vodafone.com>
2018-02-06 11:52:50 +0000

    Improving code coverage of enums

--- src/main/java/com/vodafone/charging/accountservice/controller/AccountServiceController.java
+++ src/main/java/com/vodafone/charging/accountservice/controller/AccountServiceController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.vodafone.charging.accountservice.domain.enums.ValidateHttpHeaderName.COUNTRY_HEADER_NAME;
-import static com.vodafone.charging.accountservice.domain.enums.ValidateHttpHeaderName.TARGET_HEADER_NAME;
 import static org.apache.logging.log4j.util.Strings.isNotEmpty;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -60,30 +58,4 @@ public class AccountServiceController {
             throw new MethodArgumentValidationException(iae.getMessage(), iae);
         }
     }
-
-    /**
-     * Checks that the mandatory Country and Target header exist, otherwise reject the request.
-     */
-    public void checkMandatoryHeaders(final HttpHeaders headers) {
-        try {
-            final String countryMessage = "header: " + COUNTRY_HEADER_NAME + " is mandatory";
-            final String targetMessage = "header: " + TARGET_HEADER_NAME + " is mandatory";
-
-            checkArgument(headers.get(COUNTRY_HEADER_NAME.getName()) != null, countryMessage);
-            checkArgument(headers.get(TARGET_HEADER_NAME.getName()) != null, targetMessage);
-
-            checkArgument( isNotEmpty(headers.get(COUNTRY_HEADER_NAME.getName()).stream()
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException(countryMessage))),
-                    countryMessage);
-
-            checkArgument(isNotEmpty(headers.get(TARGET_HEADER_NAME.getName()).stream()
-                    .findFirst().orElseThrow(() -> new IllegalArgumentException(targetMessage))), targetMessage);
-
-        } catch (IllegalArgumentException iae) {
-            throw new MethodArgumentValidationException(iae.getMessage(), iae);
-        }
-
-    }
-
 }
