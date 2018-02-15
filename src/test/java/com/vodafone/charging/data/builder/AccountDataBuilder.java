42a14d2435609e81236bf201ab24ec2e6681081f
Ravi Aghera <ravi.aghera@vodafone.com>
2018-02-15 17:23:07 +0000

    Adding Account data builder

---
+++ src/test/java/com/vodafone/charging/data/builder/AccountDataBuilder.java
@@ -0,0 +1,18 @@
+package com.vodafone.charging.data.builder;
+
+import com.vodafone.charging.accountservice.domain.model.Account;
+
+import java.util.Date;
+import java.util.Random;
+
+import static com.vodafone.charging.data.builder.ChargingIdDataBuilder.*;
+
+public class AccountDataBuilder {
+
+    public static Account aAccount() {
+        return Account.builder().accountId(String.valueOf(new Random().nextInt()))
+                .chargingId(aChargingId())
+                .lastValidate(new Date()).build();
+    }
+
+}
