49227841baaa02eaf80a880dff2b6ef6a90a899b
alexromanin <alex.romanin1@vodafone.com>
2018-02-15 17:28:03 +0000

    fixed versions of mongo-related deps

--- src/test/java/com/vodafone/charging/accountservice/mongo/EmbeddedMongoTest.java
+++ src/test/java/com/vodafone/charging/accountservice/mongo/EmbeddedMongoTest.java
@@ -11,6 +11,12 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * actually an integration test, will remove from develop branch
+ * Ravi Aghera @raviaghera  commented 5 minutes ago
+ This is more of an integration test, I wouldn't keep it tbh.
+ If you want to keep it then I would suggest having a new test category called "RepositoryTests" which call the Repo and have an actual DB. You would also need to add another test execution in maven to run these (probably after unit tests but before IT).
+ */
 public class EmbeddedMongoTest extends AbstractMongoTest {
 
     @Test
